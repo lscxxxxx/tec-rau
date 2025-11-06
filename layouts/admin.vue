9603a3f8d043ecad2ef7b30b3e281f4d6178b2d5
lscxxxxx <lucas.batistanunes7@gmail.com>
2025-11-06 16:18:07 -0300

    novo leiaute para o admin

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="flex h-screen bg-gray-100">
-        <aside class="w-64 flex-shrink-0 bg-gray-800 text-white flex flex-col">
+        <aside class="w-64 flex-shrink-0 bg-gray-200 uppercase text-gray-600 flex flex-col">
             <div class="h-16 flex items-center justify-center text-2xl font-bold">
-                TEC-RAU Admin
+                <a href="#"><img src="/logo.png" alt="Logo do site" class="h-10" /></a>
             </div>
             <nav class="flex-1 px-2 py-4 space-y-2">
-                <NuxtLink to="/admin/trabalhos" class="flex items-center px-4 py-2 rounded-md hover:bg-gray-700">
+                <NuxtLink to="/admin/trabalhos" class="flex items-center px-4 py-2 rounded-md hover:bg-gray-700 hover:text-white">
                     Trabalhos
                 </NuxtLink>
                 <NuxtLink to="/admin/admins" class="flex items-center px-4 py-2 rounded-md hover:bg-gray-700">
